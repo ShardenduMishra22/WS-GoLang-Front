2210fad0550dfb971bf4abdc4a0e604447d89d20
Shardendu Mishra <152168607+ShardenduMishra22@users.noreply.github.com>
2024-11-03 19:17:34 +0530

    Removed Freaking React, seriously ?

--- src/components/DowloadData.tsx
+++ src/components/DowloadData.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { CheckCircle2, Download, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
